38c78fc9207775fb9bc0da3b9aa1972c9bf56344
Angel Amaral <61510415+aamaral-msft@users.noreply.github.com>
2023-04-04 13:55:09 -0500

    Update StartVM-managed-identity.ps1

--- StartVM-managed-identity.ps1
+++ StartVM-managed-identity.ps1
@@ -1,4 +1,6 @@
-﻿# This syntax is needed to leverage the newly created managed 'system-assigned' identity
+## Ensure you create two (2) different Runbooks: (1) to turn on (2) to turn off, in your Automation Account blade
+
+# This syntax is needed to leverage the newly created managed 'system-assigned' identity
 
 # Ensures you do not inherit an AzContext in your runbook
 Disable-AzContextAutosave -Scope Process
@@ -9,11 +11,10 @@ $AzureContext = (Connect-AzAccount -Identity).context
 # Set and store context
 $AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
 
-## Start the VM
+## Start the VM (1)
 Start-AzVM -ResourceGroupName "NAME" -Name "vm-name01"
 Start-AzVM -ResourceGroupName "NAME" -Name "vm-name02"
 
-## Ensure you create two (2) different Runbooks: (1) to turn on (2) to turn off in your Automation Account blade:
-## Stop the VM
+## Stop the VM (2)
 #Stop-AzVM -ResourceGroupName "NAME" -Name "vm-name01" -Force
 #Stop-AzVM -ResourceGroupName "NAME" -Name "vm-name02" -Force
