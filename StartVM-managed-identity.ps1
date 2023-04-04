76a8326b3ae141bf25cc7233d8926d35b7744350
Amaral <61510415+aamaral-msft@users.noreply.github.com>
2023-04-04 10:15:44 -0500

    Add files via upload

---
+++ StartVM-managed-identity.ps1
@@ -0,0 +1,18 @@
+﻿# This syntax is needed to leverage the newly created managed 'system-assigned' identity
+
+# Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave -Scope Process
+
+# Connect to Azure with system-assigned managed identity
+$AzureContext = (Connect-AzAccount -Identity).context
+
+# Set and store context
+$AzureContext = Set-AzContext -SubscriptionName $AzureContext.Subscription -DefaultProfile $AzureContext
+
+## Start the VM
+Start-AzVM -ResourceGroupName "NAME" -Name "vm-name01"
+Start-AzVM -ResourceGroupName "NAME" -Name "vm-name02"
+
+## Stop the VM
+Stop-AzVM -ResourceGroupName "NAME" -Name "vm-name01" -Force
+Stop-AzVM -ResourceGroupName "NAME" -Name "vm-name02" -Force
